e5d26dfc63388cafc27ced33261a73820af57a40
José Ángel Martín Baos <JoseAngelMartinB@users.noreply.github.com>
2022-10-27 11:02:35 +0200

    Add type checking to kernel utils

--- src/gklr/kernel_utils.py
+++ src/gklr/kernel_utils.py
@@ -15,18 +15,26 @@ valid_kernel_list = kernel_type_to_class.keys()
 valid_kernel_params = ["gamma"]
 
 def convert_size_bytes_to_human_readable(size_in_bytes):
-   """ Convert the size from bytes to other units like KB, MB or GB"""
-   if size_in_bytes < 1024:
-       return (size_in_bytes, "Bytes")
-   elif size_in_bytes < (1024*1024):
-       return (np.round(size_in_bytes/1024, 2), "KB")
-   elif size_in_bytes < (1024*1024*1024):
-       return (np.round(size_in_bytes/(1024*1024), 2), "MB")
-   else:
-       return (np.round(size_in_bytes/(1024*1024*1024), 2), "GB")
-
-def elapsed_time_to_str(elapsed_time_sec):
-    if elapsed_time_sec > 60:
-        return("{time:.2f} minutes".format(time=elapsed_time_sec/60))
+    """ Convert the size from bytes to other units like KB, MB or GB"""
+    if size_in_bytes < 1024:
+        return (size_in_bytes, "Bytes")
+    elif size_in_bytes < (1024*1024):
+        return (np.round(size_in_bytes/1024, 2), "KB")
+    elif size_in_bytes < (1024*1024*1024):
+        return (np.round(size_in_bytes/(1024*1024), 2), "MB")
     else:
-        return("{time:.2f} seconds".format(time=elapsed_time_sec))
+        return (np.round(size_in_bytes/(1024*1024*1024), 2), "GB")
+
+def elapsed_time_to_str(elapsed_time_sec: float) -> str:
+    """Convert the elapsed time in seconds to a string with the appropriate units
+    
+    Args:
+        elapsed_time_sec: Elapsed time in seconds
+
+    Returns:
+        A string with the elapsed time in seconds or minutes.
+    """
+    if elapsed_time_sec > 60:
+        return f"{elapsed_time_sec/60:.2f} minutes"
+    else:
+        return f"{elapsed_time_sec:.2f} seconds"
