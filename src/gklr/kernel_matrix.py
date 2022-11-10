fac66d7aca493473ccfbd4b6544c89a02380957d
José Ángel Martín Baos <JoseAngelMartinB@users.noreply.github.com>
2022-11-10 20:36:40 +0100

    Implement Recursive ridge-leverage Nyström method
    Recursive ridge leverage score sampling algorithm for the Nyström method

--- src/gklr/kernel_matrix.py
+++ src/gklr/kernel_matrix.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_array
 from sklearn.metrics.pairwise import pairwise_kernels
-from scipy.linalg import svd
+from scipy.linalg import svd, eigvalsh
 from sklearn.cluster import KMeans, MiniBatchKMeans
 
 from .logger import *
@@ -297,6 +297,15 @@ class KernelMatrix():
 
 
 class Nystroem():
+    """Nyström approximation of a kernel matrix.
+
+    This class implements the Nyström approximation of a kernel matrix. The 
+    Nyström approximation is a method to approximate a kernel matrix K using a
+    smaller matrix, which allows to reduce the computational cost of the
+    kernel methods.
+    This class is based on the implementation of the Nyström method in the
+    scikit-learn library.
+    """
     def __init__(self,
                  kernel = "rbf",
                  *,
@@ -310,6 +319,21 @@ class Nystroem():
                  random_state = None,
                  n_jobs = None,
     ):
+        """Constructor.
+
+        Args:
+            kernel: String with the name of the kernel to be used. Default = "rbf".
+            gamma: Kernel coefficient for "rbf", "poly" and "sigmoid". Default = None.
+            coef0: Independent term in kernel function. It is only significant in "poly" and "sigmoid". Default = None.
+            degree: Degree of the polynomial kernel function ("poly"). Default = None.
+            kernel_params: Additional parameters (keyword arguments) for kernel function passed as dictionary. Default = None.
+            n_components: Number of components of the Nyström approximation. Default = 100.
+            sampling: String with the sampling method to be used for obtaining the components for the Nyström approximation.
+                Default = "uniform".
+            ridge_leverage_lambda: Lambda parameter for the `DAC-ridge-leverage` sampling method. Default = 1.
+            random_state: Random state to be used. Default = None.
+            n_jobs: Number of jobs to be used. Default = None.
+        """
         self.kernel = kernel
         self.gamma = gamma
         self.coef0 = coef0
@@ -326,6 +350,16 @@ class Nystroem():
                       y = None,
                       **fit_params,
     ):
+        """Fit the Nyström approximation to the data and obtain the Nyström approximation of the kernel matrix.
+
+        Args:
+            X: Data to be used for fitting the Nyström approximation. array-like of shape (n_samples, n_features)
+            y: Target values. Default = None.
+            fit_params: Additional parameters to be passed to the kernel function.
+
+        Returns:
+            The Nyström approximation of the kernel matrix.
+        """
         return self.fit(X, **fit_params).transform(X)
 
     def fit(self,
@@ -333,12 +367,13 @@ class Nystroem():
             y = None,
     ):
         """Fit estimator to data.
+
         Samples a subset of training points, computes kernel
         on these and computes normalization matrix.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training data.
+
+        Args:
+            X: Data to be used for fitting the Nyström approximation. array-like of shape (n_samples, n_features)
+            y: Target values. Default = None.
         """
         X = check_array(X, accept_sparse='csr')
         rnd = check_random_state(self.random_state)
@@ -370,6 +405,9 @@ class Nystroem():
             p = approx_leverage_scores/np.sum(approx_leverage_scores)  
             selected = np.random.choice(X.shape[0], size=n_components, replace=False, p=p)  
             basis = X[selected]
+        elif self.sampling == "recursive-ridge-leverage":
+            leverage_indices = self.recursive_ridge_leverage(X, n_components)
+            basis = X[leverage_indices]
         else:
             msg = "{self.sampling} is not a valid sampling strategy for Nyström method."
             logger_error(msg)
@@ -389,16 +427,15 @@ class Nystroem():
 
     def transform(self, X):
         """Apply feature map to X.
+
         Computes an approximate feature map using the kernel
         between some training points and X.
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Data to transform.
-        Returns
-        -------
-        X_transformed : ndarray of shape (n_samples, n_components)
-            Transformed data.
+
+        Args:
+            X: Data to transform. array-like of shape (n_samples, n_features)
+
+        Returns:
+            Transformed data. ndarray of shape (n_samples, n_components)
         """
         X = check_array(X, accept_sparse='csr')
         kernel_params = self._get_kernel_params()
@@ -417,22 +454,23 @@ class Nystroem():
 
     def DAC_ridge_leverage(self,
                            X,
+                           n_components,
                            lambda_,
-                           sample_size,
     ):
         """DAC ridge-leverage algorithm for Nyström approximation.
 
         This function computes an approximation of the ridge leverage score, using a divide and conquer strategy.
         Reference: Farah Cherfaoui, Hachem Kadri, Liva Ralaivola.  Scalable ridge Leverage score sampling for the 
         Nyström method. ICASSP, May 2022, Singapour, Singapore.
+        Based on the implementation of the authors: https://github.com/DAC-paper/Divide_And_Conquer_leverage_score_approximation 
 
         Args:
-            X: numpy array of size (n, d) where n is the number of data and d number of features.
-            lambda_: regularisation term.
-            sample_size: size of sub-matrix.
+            X: Numpy array of size (n, d) where n is the number of data and d number of features.
+            sample_size: Size of sub-matrix.
+            lambda_: Regularisation term.
         """
 
-        if (lambda_ < 0):
+        if lambda_ < 0:
             msg = "'lambda_' parameter of DAC_ridge_leverage have to be positive."
             logger_error(msg)
             raise ValueError(msg)
@@ -442,10 +480,10 @@ class Nystroem():
         np.random.shuffle(ind)
         approximated_ls = np.zeros((n))
 
-        for l in range(0, math.ceil(n/sample_size)):
+        for l in range(0, math.ceil(n/n_components)):
             # Sample a subset of data
-            true_sample_size = min(sample_size, n - l*sample_size)
-            temp_ind = ind[l*sample_size: l*sample_size + true_sample_size]
+            true_sample_size = min(n_components, n - l*n_components)
+            temp_ind = ind[l*n_components: l*n_components + true_sample_size]
             
             # compute the kernel matrix using the subset of selected data
             K_S = pairwise_kernels(X[temp_ind], X[temp_ind],
@@ -458,3 +496,93 @@ class Nystroem():
             approximated_ls[temp_ind] = np.sum(K_S * np.linalg.inv(K_S + lambda_ * np.eye(true_sample_size)) , axis = 1)
 
         return approximated_ls
+
+    def recursive_ridge_leverage(self,
+                                 X,
+                                 n_components,
+    ):
+        """
+        Recursive ridge leverage score sampling algorithm for the Nyström method.
+
+        This function computes an approximation of the ridge leverage score, using a recursive strategy.
+        Reference: Cameron Musco, Christopher Musco. Recursive Sampling for the Nyström Method. NIPS 2017.
+        https://doi.org/10.48550/arXiv.1605.07583
+        Based on the implementation of Axel Vanraes: https://github.com/axelv/recursive-nystrom
+
+        Args:
+            X: Numpy array of size (n, d) where n is the number of data and d number of features.
+            n_components: Size of sub-matrix.
+        """
+
+        n_oversample = np.log(n_components)
+        k = np.ceil(n_components / (4 * n_oversample)).astype(np.integer)
+        n_levels = np.ceil(np.log(X.shape[0] / n_components) / np.log(2)).astype(np.integer)
+        perm = np.random.permutation(X.shape[0])
+
+        # set up sizes for recursive levels
+        size_list = [X.shape[0]]
+        for l in range(1, n_levels+1):
+            size_list += [np.ceil(size_list[l - 1] / 2).astype(np.integer)]
+
+        # indices of poitns selected at previous level of recursion
+        # at the base level it's just a uniform sample of ~ n_component points
+        sample = np.arange(size_list[-1])
+        indices = perm[sample]
+        weights = np.ones((indices.shape[0],))
+
+        # we need the diagonal of the whole kernel matrix
+        k_diag = np.ones((X.shape[0],1))
+
+        # Main recursion, unrolled for efficiency
+        for l in reversed(range(n_levels)):
+            # indices of current uniform sample
+            current_indices = perm[:size_list[l]]
+            # build sampled kernel
+
+            # all rows and sampled columns
+            KS = pairwise_kernels(X[current_indices,:], X[indices,:],
+                                   metric=self.kernel,
+                                   filter_params=True,
+                                   n_jobs=self.n_jobs,
+                                   **self._get_kernel_params())
+            SKS = KS[sample, :] # sampled rows and sampled columns
+
+            # optimal lambda for taking O(k log(k)) samples
+            if k >= SKS.shape[0]:
+                # for the rare chance we take less than k samples in a round
+                lmbda = 10e-6
+                # don't set to exactly 0 to avoid stability issues
+            else:
+                # eigenvalues equal roughly the number of points per cluster, maybe this should scale with n?
+                # can be interpret as the zoom level
+                lmbda = (np.sum(np.diag(SKS) * (weights ** 2))
+                        - np.sum(eigvalsh(SKS * weights[:,None] * weights[None,:], eigvals=(SKS.shape[0]-k, SKS.shape[0]-1))))/k
+            lmbda = np.maximum(lmbda, 1e-7)
+
+            # compute and sample by lambda ridge leverage scores
+            R = np.linalg.inv(SKS + np.diag(lmbda * weights ** (-2)))
+            R = np.matmul(KS, R)
+            #R = np.linalg.lstsq((SKS + np.diag(lmbda * weights ** (-2))).T,KS.T)[0].T
+            if l != 0:
+                # max(0, . ) helps avoid numerical issues, unnecessary in theory
+                leverage_score = np.minimum(1.0, n_oversample * (1 / lmbda) * np.maximum(+0.0, (
+                        k_diag[current_indices, 0] - np.sum(R * KS, axis=1))))
+                # on intermediate levels, we independently sample each column
+                # by its leverage score. the sample size is n_components in expectation
+                sample = np.where(np.random.uniform(size=size_list[l]) < leverage_score)[0]
+                # with very low probability, we could accidentally sample no
+                # columns. In this case, just take a fixed size uniform sample
+                if sample.size == 0:
+                    leverage_score[:] = n_components / size_list[l]
+                    sample = np.random.choice(size_list[l], size=n_components, replace=False)
+                weights = np.sqrt(1. / leverage_score[sample])
+
+            else:
+                leverage_score = np.minimum(1.0, (1 / lmbda) * np.maximum(+0.0, (
+                        k_diag[current_indices, 0] - np.sum(R * KS, axis=1))))
+                p = leverage_score/leverage_score.sum()
+
+                sample = np.random.choice(X.shape[0], size=n_components, replace=False, p=p)
+            indices = perm[sample]
+
+        return indices
